fdcd5f2bf1d1cc67a567360fa386ac8660d7a58c
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2019-03-22 16:21:43 +0100

    added window.open()

--- script/script.js
+++ script/script.js
@@ -1,6 +1,31 @@
+let currentMode = 'light';
+
 function switchMode() {
   let body = document.getElementById('body');
 
-  body.style.backgroundColor = "#2d2f37";
-  body.style.color = "white";
+  if (currentMode === 'light') {
+    body.style.backgroundColor = "#2d2f37";
+    body.style.color = "white";
+    currentMode = 'dark';
+  } else {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+    currentMode = 'light';
+  }
+}
+
+function hoftor() {
+  window.open("http://10.1.25.62:8080/cgi-bin/tor.py?Tor=1");
+}
+
+function garage() {
+  window.open("http://10.1.25.62:8080/cgi-bin/tor.py?Tor=2");
+}
+
+function werkstatt () {
+  window.open("http://10.1.25.62:8080/cgi-bin/tor.py?Tor=3");
+}
+
+function hoftorUndGarage() {
+  window.open("http://10.1.25.62:8080/cgi-bin/tor.py?Tor=4");
 }
